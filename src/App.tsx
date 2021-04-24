b40d8e6455302148113770006d79b8f1dabf2131
Victor <victor@Victors-iMac-Pro.local>
2021-04-24 17:13:07 -0300

    first axios request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
-function App() {
+interface Asset {
+  id: number;
+  sensors: string[];
+  model: string;
+  status: string;
+  healthscore: number;
+  name: string;
+  image: string;
+  specifications: string[];
+  metrics: string[];
+}
+
+const App: React.FC = () => {
+  const [assets, setAssets] = useState<[Asset]>();
+  
+  useEffect(() => {
+    api.get('/assets').then((response) => {
+      setAssets(response.data);
+    });
+  }, []);
+  
   return (
     <div className="flex justify-center items-center h-screen">
-      <p>First react app using typescript and tailwindcss</p>
+      {
+        assets?.map((asset) => {
+          return(
+            <div className="block" key={asset.id}>
+              <p>{asset.sensors}</p>
+              <p>{asset.model}</p>
+              <p>{asset.status}</p>
+              <p>{asset.healthscore}</p>
+              <p>{asset.name}</p>
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
